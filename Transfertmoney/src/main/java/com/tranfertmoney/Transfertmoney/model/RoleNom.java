3da3407953c03ce4869943c8f0143ef16a6a3e6f
SDiene <diene.git814@gmail.com>
2019-10-02 15:38:03 +0000

    Initialisation du projet

---
+++ Transfertmoney/src/main/java/com/tranfertmoney/Transfertmoney/model/RoleNom.java
@@ -0,0 +1,9 @@
+package com.tranfertmoney.Transfertmoney.model;
+
+public enum RoleNom {
+    ROLE_USER,
+    ROLE_ADMIN,
+    ROLE_CAISSIER,
+    ROLE_PARTENAIRE,
+    ROLE_ADMIN_PARTENAIRE
+}
---
+++ Transfertmoney/src/main/java/com/tranfertmoney/Transfertmoney/model/User.java
@@ -0,0 +1,139 @@
+package com.tranfertmoney.Transfertmoney.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.hibernate.annotations.NaturalId;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "users", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {
+                "username"
+        }),
+        @UniqueConstraint(columnNames = {
+                "email"
+        })
+})
+public class User{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(min=3, max = 50)
+    private String nom;
+
+    @NotBlank
+    @Size(min=3, max = 50)
+    private String prenom;
+
+    @NotBlank
+    @Size(min=3, max = 50)
+    private String status;
+
+    @NotBlank
+    @Size(min=3, max = 50)
+    private String username;
+
+    @NaturalId
+    @NotBlank
+    @Size(max = 50)
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(min=6, max = 100)
+    private String password;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn
+    private Partenaire partenaire;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn
+    private Compte compte;
+
+    public User() {}
+
+    public User(String nom, String prenom, String status ,String username, String email, String password) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.status = status;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) { this.id = id; }
+
+    public String getNom() { return nom; }
+
+    public void setNom(String nom) { this.nom = nom; }
+
+    public String getPrenom() { return prenom; }
+
+    public void setPrenom(String prenom) { this.prenom = prenom; }
+
+    public void setStatus(String status) { this.status = status; }
+
+    public String getStatus() { return status; }
+
+    public String getUsername() { return username; }
+
+    public void setUsername(String username) { this.username = username; }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+
+    public Partenaire getPartenaire() {
+        return partenaire;
+    }
+
+    public void setPartenaire(Partenaire partenaire) {
+        this.partenaire = partenaire;
+    }
+
+    public Compte getCompte() {
+        return compte;
+    }
+
+    public void setCompte(Compte compte) {
+        this.compte = compte;
+    }
+}
